be3f46cb345442a57b6914700b5ba641550623bb
Brian Huang <brianhuang56@gmail.com>
2023-10-19 17:18:37 -0400

    Added images to accompany header information

--- src/components/ResumeHeader.jsx
+++ src/components/ResumeHeader.jsx
@@ -6,9 +6,21 @@ function ResumeHeader(props) {
         <div className="header-container">
             <div className="resume-name">{props.header.name}</div>
             <div className="resume-info">
-                <span className="phone">{props.header.phone}</span>
-                <span className="email">{props.header.email}</span>
-                <span className="location">{props.header.location}</span>
+                {props.header.email ? 
+                    <div className="header-info-container">
+                        <img src="./email.svg" height="20px"/>
+                        <span className="email">{props.header.email}</span>
+                    </div>: null}
+                    {props.header.phone ? 
+                    <div className="header-info-container">
+                        <img src="./phone.svg" height="20px"/>
+                        <span className="phone">{props.header.phone}</span>
+                    </div>: null}
+                    {props.header.location ? 
+                    <div className="header-info-container">
+                        <img src="./map-marker.svg" height="20px"/>
+                        <span className="location">{props.header.location}</span>
+                    </div>: null}
             </div>
         </div>
     );
